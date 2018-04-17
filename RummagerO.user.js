3ec39db906417a1fdad22090ec4ebc8239ecbc88
NB <asim@asimweb.org>
2018-04-17 23:07:59 +0000

    Create RummagerO.user.js
    Try to see if this works on Firefox Mobile

---
+++ RummagerO.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        RummagerO
+// @namespace   https://robotpistol.github.io/rummager
+// @include     https://bevager.com/brg/home?rewardsGroupName=rumbustion
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function rummage(){
+  document.body.appendChild(document.createElement('script')).src='https://robotpistol.github.io/rummager/rummager.js';
+  }
+rummage();
